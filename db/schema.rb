7e924850935d6c57ab5be4eae3224edf27951322
ronjeff0816 <ronjeffersonlotino@gmail.com>
2020-06-22 17:32:44 +0900

    Update schema.rb
    テーブル生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,58 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200619054251) do
+ActiveRecord::Schema.define(version: 20200620092531) do
+
+  create_table "favorites", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "shop_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shop_id"], name: "index_favorites_on_shop_id", using: :btree
+    t.index ["user_id", "shop_id"], name: "index_favorites_on_user_id_and_shop_id", unique: true, using: :btree
+    t.index ["user_id"], name: "index_favorites_on_user_id", using: :btree
+  end
+
+  create_table "shop_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "shop_id",    null: false
+    t.string   "image",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shop_id"], name: "index_shop_images_on_shop_id", using: :btree
+  end
+
+  create_table "shops", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "shop_name",                              null: false
+    t.integer  "condition",                              null: false
+    t.text     "introduction",             limit: 65535
+    t.string   "shop_tel"
+    t.string   "shop_add",                               null: false
+    t.time     "weekday_open"
+    t.time     "weekday_close"
+    t.time     "weekend_open"
+    t.time     "weekend_close"
+    t.string   "dayoff"
+    t.integer  "owner_id"
+    t.integer  "town_id",                                null: false
+    t.boolean  "corona_twoWays"
+    t.boolean  "corona_twoMeters"
+    t.boolean  "corona_partition"
+    t.boolean  "corona_disinfect"
+    t.boolean  "corona_mask"
+    t.boolean  "corona_temperature"
+    t.boolean  "corona_distance"
+    t.boolean  "corona_customerDisinfect"
+    t.boolean  "corona_customerDistance"
+    t.boolean  "corona_exit"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+  end
+
+  create_table "towns", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "town_name",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                null: false
@@ -27,4 +78,7 @@ ActiveRecord::Schema.define(version: 20200619054251) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "favorites", "shops"
+  add_foreign_key "favorites", "users"
+  add_foreign_key "shop_images", "shops"
 end
