c80a69bbcde253d37e5cf3b62e6aa72ac4f535bd
ronjeff0816 <ronjeffersonlotino@gmail.com>
2020-06-22 17:25:57 +0900

    Update 20200620092451_create_shop_images.rb
    shop_imageColums

--- db/migrate/20200620092451_create_shop_images.rb
+++ db/migrate/20200620092451_create_shop_images.rb
@@ -1,6 +1,8 @@
 class CreateShopImages < ActiveRecord::Migration[5.0]
   def change
     create_table :shop_images do |t|
+      t.references :shop, null: false, foreign_key: true
+      t.string :image, null: false
 
       t.timestamps
     end
