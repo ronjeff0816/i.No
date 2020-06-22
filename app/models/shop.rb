2ff527c59038a20a37e2a2fa7aa1a4fadbf79987
ronjeff0816 <ronjeffersonlotino@gmail.com>
2020-06-22 17:42:10 +0900

    Update shop.rb
    Association作成

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,6 @@
 class Shop < ApplicationRecord
+  has_many :favorites, dependent: :destroy
+  belongs_to :town
+  has_many :shop_images, dependent: :destroy
+  accepts_nested_attributes_for :shop_images, allow_destroy: true
 end
