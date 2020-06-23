5a14e5b1c2d102eb71da19255ba16544f73989aa
ronjeff0816 <ronjeffersonlotino@gmail.com>
2020-06-23 23:51:04 +0900

    Update shops_controller.rb
    newとcreateアクション設定

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,4 +1,5 @@
 class ShopsController < ApplicationController
+  before_action :authenticate_user!, only:[:edit]
  
   # before_action :set_user, only:[:index]
 
@@ -6,9 +7,18 @@ class ShopsController < ApplicationController
   end
 
   def new
+    @shop = Shop.new
+    # @shop.shop_images.new
   end
 
   def create
+    @shop = Shop.new(shop_params)
+    if @shop.save
+      @shop.update(owner_id: current_user.id)
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -21,5 +31,33 @@ class ShopsController < ApplicationController
   # def set_user
   #   user = User.find(params[:id])
   # end
+
+  private 
+
+  def shop_params
+    params.require(:shop).permit(
+      :shop_name, 
+      :condition, 
+      :introduction,
+      :shop_tel, 
+      :shop_add, 
+      :weekday_open, 
+      :weekday_close, 
+      :weekend_open, 
+      :weekend_close,
+      :dayoff,
+      :owner_id,
+      :town_id,
+      :corona_twoWays,
+      :corona_twoMeters,
+      :corona_partition,
+      :corona_disinfect,
+      :corona_mask,
+      :corona_temperature,
+      :corona_distance,
+      :corona_customerDisinfect,
+      :corona_customerDistance,
+      :corona_exit)
+  end
 
 end
