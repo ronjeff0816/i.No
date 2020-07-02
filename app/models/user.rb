fc1e37e782046506247e4f4ac497f4eaf4ccbb92
ronjeff0816 <ronjeffersonlotino@gmail.com>
2020-07-02 22:41:32 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :shops, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorite_shops, through: :favorites, source: :shop
   # has_one :favorite_shops, dependent: :destroy
