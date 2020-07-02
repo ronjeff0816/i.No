a4834675f49de6bcd9f09cdac2b42548312a10f8
ronjeff0816 <ronjeffersonlotino@gmail.com>
2020-07-02 22:44:50 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'favorites/create'
+  get 'favorites/destroy'
   devise_for :users
 
   root "shops#index"
   resources :users, only: [:edit, :update]
   resources :shops, only: [:index, :new, :create, :edit, :update, :show, :destroy]
+  resources :favorites, only: [:index]
+
+  resources :shops, shallow: true do
+    resource :favorites, only: %i[create destroy]
+    get :favorites, on: :collection
+  end
 
 end
